57537d4310bd0edcb7854c7c577e43d19332fd46
rohitshinde3903 <rohitshinde3903@gmail.com>
2024-12-13 18:25:41 +0530

    new modifications

--- app.py
+++ app.py
@@ -4,32 +4,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('user/index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('user/about.html')
 
 @app.route('/services')
 def services():
-    return render_template('services.html')
+    return render_template('user/services.html')
 
 @app.route('/products')
 def products():
-    return render_template('products.html')
+    return render_template('user/products.html')
 
 @app.route('/testimonials')
 def testimonials():
-    return render_template('testimonials.html')
+    return render_template('user/testimonials.html')
 
 @app.route('/contact')
 def contact():
     
-    return render_template('contact.html')
+    return render_template('user/contact.html')
 
 @app.route('/about_rohit')
 def about_rohit():
-    return render_template('about_rohit.html')
+    return render_template('user/about_rohit.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
