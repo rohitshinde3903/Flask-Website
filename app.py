229eacd04c5b9dd5665274f176209309ff31a3be
rohitshinde3903 <rohitshinde3903@gmail.com>
2024-12-13 15:34:41 +0530

    New Project

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/testimonials')
+def testimonials():
+    return render_template('testimonials.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about_rohit')
+def about_rohit():
+    return render_template('about_rohit.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./static/css/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
